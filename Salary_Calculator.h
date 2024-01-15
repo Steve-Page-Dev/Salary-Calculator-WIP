34fc7f0dad512894b92e3901c0fda09d2561da49
Steve-Page-Dev <125716932+Steve-Page-Dev@users.noreply.github.com>
2024-01-15 15:52:06 -0500

    Update Salary_Calculator.h
    Started on variable declarations for .cpp file

--- Salary_Calculator.h
+++ Salary_Calculator.h
@@ -1,1 +1,11 @@
+#include <iostream>
+#include <string>
+#pragma once
+using namespace std;
 
+//This header is used to contain the numerous variables used by the calculator for cleaner code and ease of use.
+
+//Defining variable to hold user choice for pay cycle.
+
+//Defining booleans to handle pay cycle choice.
+bool payCycleChoice = false;
